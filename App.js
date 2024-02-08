38c4e88953a08db7cd0cbb54aa387e07b55e0a2f
Pintu Gouda <pintugouda1591998@gmail.com>
2024-02-08 23:44:02 +0530

    app.js added

---
+++ App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Group from './Components/Group';
+import Index1 from './Components/Index1';
+
+function App() {
+  return (
+    <div className="App">
+     <Index1/>
+     <Group/>
+    </div>
+  );
+}
+
+export default App;
